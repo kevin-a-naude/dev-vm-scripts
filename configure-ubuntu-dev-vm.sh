f55334f223543c3f85a1844b066e3280c2134c83
Kevin A. Naudé <1943178+kevin-a-naude@users.noreply.github.com>
2023-07-14 16:05:30 +1000

    Fix generation of virtfs fstab

--- configure-ubuntu-dev-vm.sh
+++ configure-ubuntu-dev-vm.sh
@@ -5,7 +5,7 @@ function is_mounted() { # is_mounted <path>
 }
 
 function file_contains_lines() { # file_contains_lines <file-path> <lines>
-  [[ ! -z $(grep -Pzl "$2" "$1") ]] && true
+  [[ ! -z $(grep -Pzl "${2//\n/\\n}" "$1") ]] && true
 }
 
 # mount_virtiofs <share-name> <mount-path>
@@ -28,14 +28,15 @@ END
 function mount_virtfs_share() {
   local SHARE="$1"
   local MOUNT_PATH="$2"
-  local MAPPING="$(stat -c '%u' "$MOUNT_PATH")/$(stat -c '%u' "$HOME"):@$(stat -c '%g' "$MOUNT_PATH")/@$(stat -c '%g' "$HOME")"
   sudo mkdir -p "$MOUNT_PATH" && \
-  sudo mount -t 9p -o trans=virtio "$SHARE" "$MOUNT_PATH" -oversion=9p2000.L >/dev/null 2>&1 && \
-  sudo bindfs "--map=$MAPPING" "$MOUNT_PATH" "$MOUNT_PATH" && \
-  cat <<END
+  sudo mount -t 9p -o trans=virtio "$SHARE" "$MOUNT_PATH" -oversion=9p2000.L >/dev/null 2>&1 && (\
+    MAPPING="$(stat -c '%u' "$MOUNT_PATH")/$(stat -c '%u' "$HOME"):@$(stat -c '%g' "$MOUNT_PATH")/@$(stat -c '%g' "$HOME")"
+    sudo bindfs "--map=$MAPPING" "$MOUNT_PATH" "$MOUNT_PATH" && \
+    cat <<END
 $SHARE	$MOUNT_PATH	9p	trans=virtio,version=9p2000.L,rw,_netdev,nofail	0	0
 $MOUNT_PATH	$MOUNT_PATH	fuse.bindfs	map=$MAPPING	0	0
 END
+  )
 }
 
 # mount_virtiofs_or_virtfs_share <share-name> <virtiofs-mount-path> <virtfs-mount-path>
@@ -51,7 +52,7 @@ echo "1. Updating OS"
 DEBIAN_FRONTEND=noninteractive sudo apt update && DEBIAN_FRONTEND=noninteractive sudo apt full-upgrade -y >/dev/null
 
 echo "2. Installing essential tools"
-DEBIAN_FRONTEND=noninteractive sudo apt install git curl wget grep sed gnupg gpg bindfs build-essential nano micro -y >/dev/null
+DEBIAN_FRONTEND=noninteractive sudo apt install git curl wget grep sed gnupg gpg bindfs build-essential nano micro apt-utils -y >/dev/null
 
 echo "3. Mounting share (if provided)"
 FSTAB_LINES=$(mount_virtiofs_or_virtfs_share share /mnt/share "/mnt/share/$USER")
