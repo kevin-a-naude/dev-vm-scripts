15542829202894399a9e9fb294183f7aad943820
Kevin A. Naudé <1943178+kevin-a-naude@users.noreply.github.com>
2023-07-17 12:34:28 +1000

    Fix asdf data dir

--- new-dev-vm.sh
+++ new-dev-vm.sh
@@ -105,6 +105,7 @@ completed
 heading "Installing asdf"
 ASDF_DIR="$HOME/.local/asdf"
 asdf_dir="$ASDF_DIR"
+ASDF_DATA_DIR="$ASDF_DIR/data"
 if [ ! -e "$ASDF_DIR/asdf.sh" ]; then
   sudo apt install zlib1g-dev libyaml-dev
 
